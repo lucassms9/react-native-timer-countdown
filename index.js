bd8ea68c8c2b1585ed6b3d3524fb9577ac383ded
Lucas <lucassms9@hotmail.com>
2018-11-25 00:57:46 -0200

    alteracao de retorno quando tempo for 0
    alteracao de retorno quando tempo for 0

--- index.js
+++ index.js
@@ -54,14 +54,21 @@ class TimerCountdown extends React.Component {
                 return this.props.formatSecondsRemaining(milliseconds);
             }
             const remainingSec = Math.round(milliseconds / 1000);
+
             const seconds = parseInt((remainingSec % 60).toString(), 10);
             const minutes = parseInt(((remainingSec / 60) % 60).toString(), 10);
             const hours = parseInt((remainingSec / 3600).toString(), 10);
+
             const s = seconds < 10 ? '0' + seconds : seconds;
             const m = minutes < 10 ? '0' + minutes : minutes;
             let h = hours < 10 ? '0' + hours : hours;
             h = h === '00' ? '' : h + ':';
-            return h + m + ':' + s;
+
+            if (seconds == 0 && minutes == 0 && hours == 0) {
+                return 'Encerado';
+            } else {
+                return h + m + ':' + s;
+            }
         };
     }
     componentDidMount() {
@@ -91,8 +98,8 @@ class TimerCountdown extends React.Component {
         const allowFontScaling = this.props.allowFontScaling;
         const style = this.props.style;
         return (<react_native_1.Text allowFontScaling={allowFontScaling} style={style}>
-        {this.getFormattedTime(secondsRemaining)}
-      </react_native_1.Text>);
+            {this.getFormattedTime(secondsRemaining)}
+        </react_native_1.Text>);
     }
 }
 TimerCountdown.defaultProps = {
